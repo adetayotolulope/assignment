bb693b3757b7690dbbdc47d3de31091163ab0178
tolulope <adetayotolulope2016@gmail.com>
2021-11-27 15:47:15 +0200

    added taskList and Registration pages with css for them

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Header from './components/Header';
 import React from "react";
 import Footer from './components/Footer';
+import Register from './pages/Register';
+import TaskList from './pages/TaskList';
 import Login from './pages/Login';
 import { Switch, Route, Redirect } from 'react-router-dom';
  
@@ -15,7 +17,12 @@ function App() {
         <div className="container">
           <Switch>
             <Route exact path={'/login'} component={Login}/>
+            <Route exact path={'/register'} component={Register}/>
+            <Route exact path={'/'} component={TaskList}/>
             <Redirect to={'/'} from={'**'}/>
+            <Route exact path={'*'}>
+              <TaskList/>
+            </Route>
           </Switch>
           <h1>Home</h1>
         </div>
